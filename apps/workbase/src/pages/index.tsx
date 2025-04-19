cb6ef08530dbf1c31b774d884b8a807d3419dc0b
Siddartha pogula <pogulaSiddharth96@gmail.com>
2025-04-20 00:03:36 +0530

    initial-commit

---
+++ apps/workbase/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@mui/material";
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.tailwind file.
+   */
+  return (
+    <div className=" flex flex-col items-center justify-center h-screen">
+      <div>Hie there</div>
+      <Button variant="contained">Submit</Button>
+    </div>
+  );
+}
+
+export default Index;
